4a446524408b24bd16c87ca141c99d80e03e0060
rebeccaherman1@berkeley.edu <rebeccaherman1@berkeley.edu>
2015-11-05 14:07:20 -0800

    fixed swinc

--- tests/assem/swinc.s
+++ tests/assem/swinc.s
@@ -1,10 +1,10 @@
 addiu $s3 $0 16
-loop: swinc $s0 0($s0)
+loop: swinc $s0 4($s0)
 bne $s0 $s3 loop
-lw $s1 0($0)
-lw $s2 4($0)
-lw $s1 8($0)
-lw $s2 12($0)
+lw $s1 4($0)
+lw $s2 8($0)
+lw $s1 12($0)
+lw $s2 16($0)
 
 
 
